9623a0e154b167fdf3b8efa3e0d520dd380445e5
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2026-07-14 20:34:57 +0500

    fix: number input type

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title("DA11 Salary Predictor")
 with open("model.pkl", "rb") as file:
     model = pickle.load(file)
 
-yoe = st.number_input(label="Enter years of experience", min_value=1, max_value=10)
+yoe = st.number_input(label="Enter years of experience", min_value=1.0, max_value=10.0, value=3.0, step=0.5)
 
 if st.button("Predict"):
     prediction = model.predict([[yoe]])
